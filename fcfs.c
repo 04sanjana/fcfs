f787514c36dc1225efcf3ce949bcc01b6af2c1c5
04sanjana <132557573+04sanjana@users.noreply.github.com>
2024-07-03 22:13:38 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -1,98 +1,106 @@
 #include<stdio.h>
-#include<string.h>
 struct process
 {
-    int at,bt,ct,tt,wt;
-    char name[10];
-}p[10],temp,g[10];
+	int ct,bt,tt,wt,at;
+	char name[20];
+}p[20],temp;
 void main()
 {
-    int i,j,n;
-    printf("enter number of processes\n");
-    scanf("%d",&n);
-    printf("enter process details\n");
-    for(i=0;i<n;i++)
-    {
-        printf("enter name of process\n");
-        scanf("%s",&p[i]name);
-        printf("Enter arrival time\n");
-        scanf("%d",&p[i].at);
-        printf("enter burst time\n");
-        scanf("%d",&p[i].bt);
-    }
-    printf("process table before sorting\n");
-    printf("--------------------------------------------------------------\n");
-    printf("name\tat\tbt\n");
-    for(i=0;i<n;i++)
-    {
-        scanf("%s%d%d",p[i].name,p[i].at,p[i].bt);
-    }
-    for(i=0;i<n;i++)
-    {
-        for(j=i+1;j<n;j++)
-        {
-            if(p[i].at>p[j].at)
-            {
-                temp=p[i];
-                p[i]=p[j];
-                p[j]=temp;
-            }
-        }
-    }
-    while(i<n)
-    {
-        if(time>=p[i].at)
-       {
-        j=i+1
-        while(j<n)
-        {
-            if(p[i].bt<p[j].bt && time>=p[j].at)
-            {
-                temp=p[i];
-                p[i]=p[j];
-                p[j]=temp;
-            }
-            j++;
-        }
-        time=time+p[i].bt;
-        p[i].ct=time;
-        p[i].tt=p[i].bt-p[i].at;
-        p[i].wt=p[i].wt-p[i].at;
-        turn=turn+p[i].tt;
-        wait=wait+p[i].wt;
-       }
-       else
-       {
-        time=p[i].at;
-       }
-
-    }
-    printf("aftr sorting\n");
-    printf("name\tat\tbt\tct\ttt\twt\n");
-    for(i=0;i<n;i++)
-    {
-        printf("%s%d%d%d%d%d",p[i].name,p[i].at),p[i].bt,p[i].ct,p[i].tt,p[i].wt\n");
-
-    }
-    printf("gantt chart\n");
-    for(i=0;i<n;i++)
-   {
-    if(g[i].bt==0)
-    {
-        printf("idle");
-
-    }
-    else
-    {
-        printf("%s",p[i].name);
-    }
-   }
-   printf("|\n");
-   printf("0\t");
-    for(i=0;i<n;i++)
-    {
-        printf("%d\t",g[i].ct);
-    }
+	int i=0,j=0,k=0,n,time=0,gtime=0;
+	float sum_tt=0,sum_wt=0;
+	printf("enter number of processes\n");
+	scanf("%d",&n);
+	printf("Enter details of processes\n");
+	for(i=0;i<n;i++)
+	{
+		printf("ENterprocess name\n");
+		scanf("%s",p[i].name);
+		printf("Enter process AT\n");
+		scanf("%d",&p[i].at);
+		printf("Enter process BT\n");
+		scanf("%d",&p[i].bt);
+	}
+	
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(p[i].at>p[j].at)
+			{
+				temp=p[i];
+				p[i]=p[j];
+				p[j]=temp;
+			}
+		}
+	}
+	i=0,j=0;
+	
+	while(i<n)
+	{
+		if(time>=p[i].at)
+		{
+			time+=p[i].bt;
+			p[i].ct=time;
+			p[i].tt=p[i].ct-p[i].at;
+			p[i].wt=p[i].tt-p[i].bt;
+			sum_tt+=p[i].tt;
+			sum_wt+=p[i].wt;
+			printf("\n%s\t%d\t%d\t%d\t%d\t%d\n",p[i].name,p[i].at,p[i].bt,p[i].ct,p[i].tt,p[i].wt);
+			i++;
+			
+		}
+		else
+		{
+			time=p[i].at;
+		}
+	}
+	i=0,j=0;
+	printf("\nupdated\n");
+	printf("pid\tat\tbt\tct\ttt\twt\n");
+	for(i=0;i<n;i++)
+	{
+		printf("%s\t%d\t%d\t%d\t%d\t%d\n",p[i].name,p[i].at,p[i].bt,p[i].ct,p[i].tt,p[i].wt);
+	}
+		
+	printf("Gantt Chart\n");
+	while(k<n)
+	{
+		if(gtime>=p[i].at)
+		{
+			printf("|%s\t",p[k].name);
+			gtime += p[k].bt;
+			k++;
+		}
+		else
+		{
+			printf("|idle\t");
+			gtime=p[i].at;
+			i++;
+		}
+	}
+	printf("|\n|0\t|");
+	k=0,gtime=0;
+	while(k<n)
+	{
+		if(gtime>=p[k].at)
+		{
+			printf("%d\t",p[k].ct);
+			gtime+=p[k].bt;
+			k++;
+		}
+		else
+		{
+			gtime=p[k].at;
+			printf("%d\t",gtime);
+			
+			i++;
+			
+		
+		}
+	}
+	printf("\nsum tt=%f\n",sum_tt/n);
+	printf("sum wt=%f\n",sum_wt/n);
 }
+	
 
 
